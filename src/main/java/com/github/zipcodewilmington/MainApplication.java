6a3c27c705f74d5742313aba7ae36f64f2a863ad
David <david@zipcoders-mbp-7.lan>
2023-03-21 17:39:34 -0400

    cards and decks

--- src/main/java/com/github/zipcodewilmington/MainApplication.java
+++ src/main/java/com/github/zipcodewilmington/MainApplication.java
@@ -6,8 +6,6 @@ import com.github.zipcodewilmington.casino.Suits;
 
 public class MainApplication {
 
-    Deck testDeck = new Deck();
-    Card aCard = new Card(Suits.CLUBS,);
     public static void main(String[] args) {
         new Casino().run();
     }
