823ed70d8db3fc3c5024ebc41b6953ef662db70a
Nina <nina.emiko.mcnair@gmail.com>
2023-03-21 19:38:11 -0400

    Added empty test methods for casino account

--- src/test/java/com/github/zipcodewilmington/testCasino/TestCasinoAccount.java
+++ src/test/java/com/github/zipcodewilmington/testCasino/TestCasinoAccount.java
@@ -1,4 +1,43 @@
 package com.github.zipcodewilmington.testCasino;
 
+import org.junit.Test;
+
 public class TestCasinoAccount {
+
+    @Test
+    public void TestCasinoAccountConstructors(){
+
+    }
+    @Test
+    public void TestCasinoAccountNullaryConstructor(){
+
+    }
+    @Test
+    public void TestCasinoAccountConstructorWithNameAndPassword(){
+
+    }
+    @Test
+    public void TestGetCasinoAccountName(){
+
+    }
+    @Test
+    public void TestSetCasinoAccountName(){
+
+    }
+    @Test
+    public void TestGetCasinoAccountPassword(){
+
+    }
+    @Test
+    public void TestSetCasinoAccountPassword(){
+
+    }
+    @Test
+    public void TestGetCasinoAccountBalance(){
+
+    }
+    @Test
+    public void TestSetCasinoAccountBalance(){
+
+    }
 }
