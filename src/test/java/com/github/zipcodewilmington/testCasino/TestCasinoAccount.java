9de81c4d4f0f1014393452f6b9f5766f6e988f7c
Nina <nina.emiko.mcnair@gmail.com>
2023-03-21 19:44:27 -0400

    Completed constructor test methods for casino account

--- src/test/java/com/github/zipcodewilmington/testCasino/TestCasinoAccount.java
+++ src/test/java/com/github/zipcodewilmington/testCasino/TestCasinoAccount.java
@@ -1,19 +1,49 @@
 package com.github.zipcodewilmington.testCasino;
 
+import com.github.zipcodewilmington.casino.CasinoAccount;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TestCasinoAccount {
 
     @Test
     public void TestCasinoAccountConstructors(){
-
+        CasinoAccount casinoAccount = new CasinoAccount(100.00, "Nina", "password");
+        double expectedBalance = 100.00;
+        String expectedName = "Nina";
+        String expectedPassword = "password";
+        double actualBalance = casinoAccount.getBalance();
+        String actualName = casinoAccount.getName();
+        String actualPassword = casinoAccount.getPassword();
+        Assert.assertEquals(expectedBalance, actualBalance);
+        Assert.assertEquals(expectedName, actualName);
+        Assert.assertEquals(expectedPassword, actualPassword);
     }
     @Test
     public void TestCasinoAccountNullaryConstructor(){
-
+        CasinoAccount casinoAccount = new CasinoAccount();
+        double expectedBalance = 0.00;
+        String expectedName = "";
+        String expectedPassword = "";
+        double actualBalance = casinoAccount.getBalance();
+        String actualName = casinoAccount.getName();
+        String actualPassword = casinoAccount.getPassword();
+        Assert.assertEquals(expectedBalance, actualBalance);
+        Assert.assertEquals(expectedName, actualName);
+        Assert.assertEquals(expectedPassword, actualPassword);
     }
     @Test
     public void TestCasinoAccountConstructorWithNameAndPassword(){
+        CasinoAccount casinoAccount = new CasinoAccount("Nina", "password");
+        double expectedBalance = 0.00;
+        String expectedName = "Nina";
+        String expectedPassword = "password";
+        double actualBalance = casinoAccount.getBalance();
+        String actualName = casinoAccount.getName();
+        String actualPassword = casinoAccount.getPassword();
+        Assert.assertEquals(expectedBalance, actualBalance);
+        Assert.assertEquals(expectedName, actualName);
+        Assert.assertEquals(expectedPassword, actualPassword);
 
     }
     @Test
