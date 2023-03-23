855b332d5b161b4e60108faa8c915bac585f1c3d
seth <sprentice97@gmail.com>
2023-03-22 23:49:00 -0400

    wrote most of slot machine

--- src/main/java/com/github/zipcodewilmington/casino/games/slots/SlotsGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/slots/SlotsGame.java
@@ -4,8 +4,11 @@ import com.github.zipcodewilmington.casino.BettingGame;
 import com.github.zipcodewilmington.casino.Game;
 import com.github.zipcodewilmington.casino.Player;
 
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Scanner;
+import java.util.function.Supplier;
 
 /**
  * Created by leon on 7/21/2020.
@@ -36,50 +39,72 @@ public class SlotsGame implements Game, BettingGame {
 
     @Override
     public void run() {
-
+        Scanner in = new Scanner(System.in);
+        String command;
+        int val;
+        while(true){
+            System.out.println("Welcome: would you like to spin the slots?\n1: spin\n2: leave");
+            command = in.next().trim();
+            if(command.equals("1")){
+                try{
+                    val = in.nextInt();
+                }catch(NumberFormatException e){
+                    System.out.println("Please enter a number");
+                    continue;
+                }
+                bet(this.player, val, 1);
+                //this.player.getAccount().setBalance()+= resolve(pull()); //I am aware this is wrong, but its colseish
+            } else if(command.equals("2")){
+                remove(this.player);
+                break;
+            }
+        }
     }
 
     @Override
     public boolean bet(Player player, int amount, int minimum) {
         if(amount < minimum){
             return false;
-        }else if(amount>player.casinoAccount.getBalance()){
+        }else if(amount>player.getAccount().getBalance()){
             return false;
         }else{
-            player.casinoAccount.setBalance(player.casinoAccount.getBalance()-amount);
+            player.getAccount().setBalance(player.getAccount().getBalance()-amount);
             this.pool = amount;
             return true;
         }
     }
 
-    public int resolve(int[] ints) {
-        
+    public int resolve(int[] slots) {
+        if(slots[0] == slots[1] && slots[1] == slots[2]){ //all numbers the same
+            return slots[0];
+        } else if(slots[2]%slots[0] == 0 && slots[1]%slots[0] == 0){ //2,4,6 or 3,6,9
+            return slots[2] * 2;
+        }else if(slots[0]%slots[2] == 0 && slots[1]%slots[2] == 0){ //reverse of above
+            return slots[0] * 2;
+        }else if(slots[0]+1 == slots[1] && slots[1]+1 == slots[2]){ //counting sequence
+            return slots[0]+1;
+        }
         return 0;
     }
 
     public int[] pull() {
-        Random rand = new Random();
-        int slot1 = 0;
-        int slot2 = 0;
-        int slot3 = 0;
-
+        Supplier<Integer> slot = () ->{
+            return (int) (Math.random() * (10-1) + 1);
+        };
+        int[] slots = new int[3];
         try {
             for (int i = 35; i <= 100; i++) {
                 Thread.sleep(80);
-                if (i < 66) {
-                    slot1 = rand.nextInt(9) + 1;
-
-                }
-                if (i < 80) {
-                    slot2 = rand.nextInt(9) + 1;
-                }
-                slot3 = rand.nextInt(9) + 1;
-                System.out.print("[" + slot1 + "]" + "[" + slot2 + "]" + "[" + slot3 + "]\r");
+                slots[0] = (i<66) ? slot.get() : slots[0];
+                slots[1] = (i<80) ? slot.get() : slots[1];
+                slots[2] = slot.get();
+                System.out.printf("[%s][%s][%s]\r", slots[0], slots[1], slots[2]);
             }
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-        return null;
+        return slots;
     }
 }
+
 
