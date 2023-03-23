49388b4e3d289823f1de114654713e50c93a7a90
Jacob C <jacobc@Zipcoders-MacBook-Pro.local>
2023-03-23 15:25:58 -0400

    CardPlayer tests and methods

--- src/main/java/com/github/zipcodewilmington/casino/CardPlayer.java
+++ src/main/java/com/github/zipcodewilmington/casino/CardPlayer.java
@@ -4,8 +4,15 @@ import java.util.ArrayList;
 
 public class CardPlayer extends Player {
 
-    ArrayList<Card> hand;
+    ArrayList<Card> hand = new ArrayList<>();
     public CardPlayer(CasinoAccount casinoAccount) {
         super(casinoAccount);
+    }
+    public ArrayList<Card> getHand(){
+        return this.hand;
+    }
+    public ArrayList<Card> addPlayerHand(Card card){
+        hand.add(card);
+        return this.hand;
     }
 }
