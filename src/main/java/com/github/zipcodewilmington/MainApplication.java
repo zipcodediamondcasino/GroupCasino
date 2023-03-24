19286853fb74554c536f274e41bb3e622f77b572
ule270 <u.le270@gmail.com>
2023-03-24 15:13:16 -0400

    changes

--- src/main/java/com/github/zipcodewilmington/MainApplication.java
+++ src/main/java/com/github/zipcodewilmington/MainApplication.java
@@ -3,11 +3,14 @@ package com.github.zipcodewilmington;
 
 import com.github.zipcodewilmington.casino.Deck;
 import com.github.zipcodewilmington.casino.games.blackjack.BlackjackHand;
+import com.github.zipcodewilmington.casino.games.oldmaid.OldMaidDealer;
+import com.github.zipcodewilmington.casino.games.oldmaid.OldMaidGame;
 
 import java.util.logging.Handler;
 
 public class MainApplication {
     public static void main(String[] args) {
-        new Casino().run();
+//        new Casino().run();
+        new OldMaidGame().run();
     }
 }
